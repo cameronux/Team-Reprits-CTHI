c39098458e193ce12f86bc0bdea901bbdc12f15f
Alyana Soriano <127061416+Yana-eyy@users.noreply.github.com>
2023-03-06 11:16:51 +0800

    Update PigGame.java
    added scanner

--- PigGame.java
+++ PigGame.java
@@ -1,10 +1,16 @@
 /**
  * @author Alyana Soriano
  */
+import java.util.*;
 public class PigGame {
     
     public static void main (String []args){
         int tab;
+        Scanner pog = new Scanner (System.in);
+        System.out.println("Insert ");
+        tab = pog.nextInt();
+        System.out.println(tab);
+
 
 
     }
